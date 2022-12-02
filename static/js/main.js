c1bd293b9b3f4cecbe6269767f7388c6567388d8
suin <parksuin55@gmail.com>
2022-12-02 16:14:01 +0900

    Feat: 프로필 페이지 탭 생성, 개인 정보 수정 탭 내용 생성
    1. 프로필 페이지 탭 생성
    	- 사이드 네비바 생성하여 탭 기능 구현
    2. 개인 정보 수정 탭의 내용 생성
    3. style.css root 색상 추가

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,15 @@ window.addEventListener('scroll', function() {
     var scrolled = scrollY >= 10; //스크롤된 상태; true or false
     document.querySelector("header").classList.toggle('header_sticky', scrolled);
 });
+
+// profile tab jquery
+$('.tab_item').on("click",function(e){
+    console.log("sss")
+    e.preventDefault(); 
+    const target_content_id = $(this).find("a").attr("href");
+    console.log(target_content_id)
+    $(".tab_item").removeClass('active');
+    $(this).addClass("active");
+    $(".tab_content").removeClass("active");
+    $(target_content_id).addClass("active");
+})
