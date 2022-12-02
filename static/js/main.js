38901e1cba58351955480dc6cd10fd41cdbf6d99
suin <parksuin55@gmail.com>
2022-12-02 17:24:00 +0900

    불필요한 코드(console.log)삭제, title 수정

--- static/js/main.js
+++ static/js/main.js
@@ -6,10 +6,8 @@ window.addEventListener('scroll', function() {
 
 // profile tab jquery
 $('.tab_item').on("click",function(e){
-    console.log("sss")
     e.preventDefault(); 
     const target_content_id = $(this).find("a").attr("href");
-    console.log(target_content_id)
     $(".tab_item").removeClass('active');
     $(this).addClass("active");
     $(".tab_content").removeClass("active");
