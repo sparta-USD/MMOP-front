7201555333720ce1c496574294ef7f17090b9cc8
haeminchoi2 <sjhk02@gmail.com>
2022-12-09 02:52:58 +0900

    Feat: 카카오로그인 API 연결 #22
    - Back-end 카카오로그인 API와 연결했습니다.
        - 인가코드 정상적으로 받았을 때와 에러 발생했을 때 나눠서 처리했습니다.
        - Back-end에서 카카오 access토큰 요청과 유저정보 요청중에 에러 발생시 에러처리 완료했습니다.
    - 정상정으로 기능이 동작하면, jwt를 받아와서 로컬스토리지에 저장합니다.

--- static/js/signin.js
+++ static/js/signin.js
@@ -35,5 +35,56 @@ async function handleSignin(){
         location.href = "/";
     }).catch(error => {
         alert("로그인에 실패하였습니다. 이메일과 비밀번호를 확인해주세요.")
+        alert("발송된 이메일을 확인해주세요!")
+        console.error(error.message)
     })
+};
+
+const REST_API_KEY = "0240e7e88dd3a8d26072a3070fda2b02";
+const REDIRECT_URI = "http://127.0.0.1:5500/signin.html";
+
+function kakaoSignin(){
+    location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
+};
+
+window.onload = async () => {
+    let params = new URL(window.location.href).searchParams;
+    // 인가코드 받아왔을 때
+    if(params.has("code")){
+        const response = await fetch("http://127.0.0.1:8000/users/oauth/callback/kakao/", {
+            headers: {
+                "content-type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify({
+                "code":params.get("code"),
+            })
+        }).then(response => {
+            // 카카오 access토큰 요청/유저정보 요청 중에 에러발생시
+            if(!response.ok){
+                return response.json().then(error_json => {throw new Error(`${error_json.error_code} 에러 발생했습니다.`)})
+            }
+            return response.json()
+        }).then(result => {
+            const base64Url = result.access.split(".")[1];
+            const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+            const jsonPayload = decodeURIComponent(atob(base64).split("").map(function (c) {
+                return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+            }).join(""));
+            localStorage.setItem("payload", jsonPayload);
+            localStorage.setItem("email", JSON.parse(jsonPayload).email);
+            localStorage.setItem("username", JSON.parse(jsonPayload).username);
+            localStorage.setItem("user_id", JSON.parse(jsonPayload).user_id);
+            
+            alert("로그인 되었습니다!");
+            location.href = "/";
+        }).catch(error => {
+            console.error(error.message+" 운영자에게 문의해주세요.")
+        })
+        // 인가코드 요청시 오류발생 했을 때
+    }else if(params.has("error")){
+        let error_code = params.get("error");
+        alert("카카오 로그인에 실패했습니다.")
+        console.error(`에러코드: ${error_code} 운영자에게 문의해주세요.`)
+    }
 };
