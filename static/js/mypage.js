a53a96946c3283059d4bb0d534b73b54aa86e021
suin <parksuin55@gmail.com>
2022-12-09 20:18:51 +0900

    Feat: 마이페이지 tab url 수정
    1. tab 클릭 시 해당 tab id를 url에 추가

--- static/js/mypage.js
+++ static/js/mypage.js
@@ -1,6 +1,44 @@
+window.addEventListener('scroll', function() {
+    const scrollY = this.window.pageYOffset;
+    var scrolled = scrollY >= 10; //스크롤된 상태; true or false
+    document.querySelector("header").classList.toggle('header_sticky', scrolled);
+});
+// profile tab jquery
+$('.tab_item').on("click",function(e){
+    let target_content_id = $(this).find("a").attr("href").replace("#","#tab_");
+    $(".tab_item").removeClass('active');
+    $(this).addClass("active");
+    $(".tab_content").removeClass("active");
+    $(target_content_id).addClass("active");
+})
+
+$(".none_link").on("click",function(e){
+    e.preventDefault();
+});
+
+
 document.addEventListener("DOMContentLoaded", function () {
+    tab_change(getURL())
     handleMock()
 });
+// * url을 불러오는 함수 *
+function getURL(){
+    const url = window.location.href
+    const get_url = url.split('#')[1]
+    return get_url;
+}
+function tab_change(get_url){
+    if(["my_perfume","my_review","like_perfume","profile_edit"].includes(get_url)){
+        console.log(get_url)
+        $(".tab_item").removeClass('active');
+        $("#tab_menu_"+get_url).addClass("active");
+        $(".tab_content").removeClass("active");
+        $("#"+get_url).addClass("active");
+    }else{
+        $(".tab_item:first-child").addClass("active");
+        $(".tab_content:first-child").addClass("active");
+    }
+}
 
 async function handleMock() {
     const response = await fetch('http://127.0.0.1:8000/users/', {
@@ -12,7 +50,7 @@ async function handleMock() {
         if(!response.ok){
             if(response.status==401){
                 alert("로그인 유저만 접근 가능합니다.")
-                location.href="/signin.html";
+                // location.href="/signin.html";
             }
             throw new Error(`${response.status} 에러가 발생했습니다.`);    
         }
