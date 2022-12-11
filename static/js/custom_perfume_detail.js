06a73574e67176b7dd5a6e626708f17c26aa8141
haeminchoi2 <sjhk02@gmail.com>
2022-12-12 02:25:32 +0900

    Feat: 커스텀 향수 상세페이지 API연결 #56
    - 커스텀 향수 상세페이지 조회 API 연결하였습니다.
        - 각 HTML태그마다 알맞는 정보 넣어주었습니다.

---
+++ static/js/custom_perfume_detail.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+    handleCustomDetail()
+});
+
+function changeDateTimeFormat(datetime){
+    const TIME_ZONE = 3240 * 10000;
+    const date = new Date(datetime)
+    return new Date(+date + TIME_ZONE).toISOString().replace('T', ' ').replace(/\..*/, '');
+}
+
+async function handleCustomDetail(){
+    console.log(document.querySelector(".perfume_creator").innerText)
+    const response = await fetch(`http://127.0.0.1:8000/custom_perfume/1`, {
+        headers: {
+            "content-type": "application/json",
+        },
+        method: "GET",
+    }).then(response => {
+        if(!response.ok){
+            throw new Error(`${response.status}`)
+        }
+        return response.json()
+    }).then(result => {
+        console.log(result)
+        console.log(document.querySelectorAll(".material img"))
+        document.querySelector(".perfume_image").src = result.package.image
+        document.querySelector(".logo_image").src = `http://127.0.0.1:8000/${result.logo}`
+        document.querySelector(".perfume_creator").innerText = result.creator_username
+        document.querySelector(".perfume_title").innerText = result.title
+        document.querySelector(".perfume_create").innerText = changeDateTimeFormat(result.created_at)
+        document.querySelector(".note1").innerText = result.note01.kor_name
+        document.querySelector(".note2").innerText = result.note02.kor_name
+        document.querySelector(".note3").innerText = result.note03.kor_name
+        document.querySelector(".note3").innerText = result.note03.kor_name
+        document.querySelector(".pakage_name").innerText = result.package.name
+        document.querySelector(".note_img1 img").src = result.note01.image
+        document.querySelector(".note_img2 img").src = result.note02.image
+        document.querySelector(".note_img3 img").src = result.note03.image
+    }).catch(error => {
+        console.error(error.message)
+    })
+};
