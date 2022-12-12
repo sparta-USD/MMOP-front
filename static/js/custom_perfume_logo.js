d9eaec2c6d8a2568ef300c83fd941abc6848e861
dy <kvkvd12@gmail.com>
2022-12-12 12:19:29 +0900

    Feat: 커스텀 향수 제작 페이지 API 연결 #28
    1. step3 에서 로고, 이름 작성후 제품만들기 버튼 클릭시 POST
    2. step3, step4 -> 하나로 합침
        - 로고, 향수 이름 한번에 작성후 제작
    3. custom_perfume_name -> custom_perfume_complete 로 변경
    4. custom_perfume_complete 에서 완성된 향수 보여줌
    5. custom_perfume_complete 에서 완료 버튼 누를시 상세페이지 이동
    
    Close #28

--- static/js/custom_perfume_logo.js
+++ static/js/custom_perfume_logo.js
@@ -3,19 +3,34 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 async function handlePackage() {
-    const response = await fetch('http://127.0.0.1:8000/custom_perfume/custom/', {
-        method: 'GET',
-        headers: {
-        },
-    }).then(response => {
-        return response.json()
-    }).then(result => {
-        const response_json = result;
-        document.getElementById("package").innerHTML = '<img class="package" id="package" aria-hidden="false" draggable="false" loading="lazy" src="'+response_json['packages'][JSON.parse(sessionStorage.getItem("package_"))-1]['image']+'">'
-    })
+    if(sessionStorage.length < 5){
+        alert("이전 step을 완료 해야됩니다!")
+        location.href="/custom_perfume_package.html"
+    }else{
+        const response = await fetch('http://127.0.0.1:8000/custom_perfume/custom/', {
+            method: 'GET',
+            headers: {
+                // "Authorization": "Bearer " + localStorage.getItem("access"),
+            },
+        }).then(response => {
+            if(!response.ok){
+                if(response.status==401){
+                    alert("로그인 유저만 접근 가능합니다.")
+                    history.back()
+                }
+                throw new Error(`${response.status} 에러가 발생했습니다.`);    
+            }
+            return response.json()
+        }).then(result => {
+            const response_json = result;
+            document.getElementById("package").innerHTML = '<img class="package" id="package" aria-hidden="false" draggable="false" loading="lazy" src="'+response_json['packages'][JSON.parse(sessionStorage.getItem("package"))-1]['image']+'">'
+            document.getElementById("note01").innerHTML = '<img class="note" src="'+response_json['notes'][JSON.parse(sessionStorage.getItem("note01"))-1]['image']+'">'
+            document.getElementById("note02").innerHTML = '<img class="note" src="'+response_json['notes'][JSON.parse(sessionStorage.getItem("note02"))-1]['image']+'">'
+            document.getElementById("note03").innerHTML = '<img class="note" src="'+response_json['notes'][JSON.parse(sessionStorage.getItem("note03"))-1]['image']+'">'
+        })
+    }
+    
 }
-
-var formData = new FormData();
 
 function readImage(input) {
     if (input.files && input.files[0]) {
@@ -25,14 +40,6 @@ function readImage(input) {
             previewImage.src = e.target.result
         }
         reader.readAsDataURL(input.files[0])
-        formData.append("logo", input.files[0])
-        sessionStorage.setItem("formData", JSON.stringify(formData));
-        for (var pair of formData.entries()) {
-            console.log(pair[0]+ ', ' + pair[1]); 
-        }
-        console.log(input.files[0])
-        console.log(formData)
-        // sessionStorage.setItem("formData", new Blob([ JSON.stringify(logo) ], {type : "application/json"}));
     }
 }
 const inputImage = document.getElementById("input-file")
@@ -40,6 +47,47 @@ inputImage.addEventListener("change", e => {
   readImage(e.target)
 })
 
-function handleNext(){
-    location.href="/custom_perfume_name.html"
+async function handleCreateUnft() {
+
+    const custom_perfume_formData = new FormData();
+
+    const title = document.getElementById("title").value;
+    const logo = document.getElementById("input-file").files[0];
+
+    custom_perfume_formData.append("title", title);
+    custom_perfume_formData.append("note01", JSON.parse(sessionStorage.getItem("note01")));
+    custom_perfume_formData.append("note02", JSON.parse(sessionStorage.getItem("note02")));
+    custom_perfume_formData.append("note03", JSON.parse(sessionStorage.getItem("note03")));
+    custom_perfume_formData.append("package", JSON.parse(sessionStorage.getItem("package")));
+    custom_perfume_formData.append("logo", logo);
+
+    if (title == "" || logo == "") {
+        alert("빈칸을 채워주세요!")
+    }
+    else {
+        const response = await fetch('http://127.0.0.1:8000/custom_perfume/custom/',{
+            method: 'POST',
+            headers: {
+                // "Authorization":"Bearer " + localStorage.getItem("access"),
+                "Authorization":"Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcwODUxMDgzLCJpYXQiOjE2NzA4MDc4ODMsImp0aSI6Ijg5MWFhZWU2YmQ4ZTQ4ZThiNmFmNTJlMjBlNWExNGNmIiwidXNlcl9pZCI6MSwiaWQiOjEsImVtYWlsIjoiYUBhLmNvbSIsInVzZXJuYW1lIjoiYSJ9.bVuGKK4LITnhPrxf-4OAE7lzObArYrQXIuTvxbh2Uf0",
+            },
+            body: custom_perfume_formData,
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error(`${response.status} 에러가 발생했습니다.`);
+            }
+            return response.json()
+        }).then(result => {
+            alert("향수 생성에 성공했습니다!")
+            location.href="/custom_perfume_complete.html?custom_perfume="+result.id
+        }).catch(error => {
+            alert("향수 생성에 실패하였습니다. \n 자세한 내용은 관리자에게 문의해주세요!");
+            console.warn(error.message);
+        });
+    }
 }
+
+document.getElementById("create_button").addEventListener("click", function () {
+    handleCreateUnft();
+});
+
