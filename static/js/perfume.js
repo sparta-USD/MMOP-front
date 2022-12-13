9b4bd2df01fd7b75c49c29553a1833b7b2bcd51f
LeeHyunji <hyunji015@gmail.com>
2022-12-14 02:10:28 +0900

    Design: 향수 상세페이지 perfume.html UI 수정
    1. style.css 공용 UI 적용 및 perfume.css 변경
    	- 불필요하거나 겹치는 내용 삭제
    	- 부적절한 스타일 삭제
    
    2. 데이터 로딩하면서 깜빡임 해결
    	- 기존 HTML 정보 삭제로 깜빡임 해결
    
    3. 리뷰 탭 정보 수정
    	- 리뷰 평점 : 평균점수 / 5 로 표현
    	- 리뷰 없을 경우 : "아직 작성된 리뷰가 없습니다." 표시
    
    4. 추천탭 - 카드 출력 한 줄에 3개씩 -> 4개씩 출력으로 변경

--- static/js/perfume.js
+++ static/js/perfume.js
@@ -39,16 +39,22 @@ async function handlePerfumeInfo(){
 // 1. 기본 향수제품정보 불러오기
 function perfume_info(data){
     const element = document.querySelector(".container_perfume_detail");
-    element.querySelector(".perfume_image").setAttribute('src', data['image']);
+    element.querySelector(".perfume_image_box").innerHTML=`
+        <img class="perfume_image" src="${data['image']? data['image']: "/static/images/perfume.png"}">
+    `;
     element.querySelector(".perfume_id").innerText = "#"+data['id'];
     element.querySelector(".perfume_brand").innerText = data['brand'];
     element.querySelector(".perfume_title").innerText = data['title'];
     element.querySelector(".likes_count").innerText = "  " +data['likes_count'];
-    element.querySelector(".col_price_1").innerText = data['price'];
+    if(data['price']){
+        element.querySelector(".col_price_1").innerText = "$ "+data['price'];
+    }else{
+        element.querySelector(".detail_price").remove();
+    }
     
     // 출시일자
     if(data['launch_date']){
-        element.querySelector(".col_launch_1").innerText = data['launch_date'].split("-")[0];
+        element.querySelector(".col_launch_1").innerText = data['launch_date'].split("-")[0]+"년";
     }else{
         element.querySelector(".detail_launch").remove();
     }
@@ -64,7 +70,7 @@ function perfume_info(data){
     // 찜 상태 표시
     const user_email = localStorage.getItem("email");
     let is_like = data['likes'].includes(user_email); // 현재 로그인한 유저의 이메일이 likes에 있는지 체크/ 찜 상태 : T/F
-    document.getElementById("btn_heart").classList.add(is_like ? "bi-suit-heart-fill" : "bi-suit-heart"); // 삼항연산자 사용!
+    document.getElementById("btn_heart").classList = is_like ? "bi bi-suit-heart-fill" : "bi bi-suit-heart"; // 삼항연산자 사용!
     
     // 리뷰 작성하기 버튼 활성화/비활성화(작성 여부에 따라)
     review_create_go(data);
@@ -97,10 +103,10 @@ function perfume_detail_tab(data){
     element.querySelector(".tab_perfume_brand").innerText = data['brand'];
     element.querySelector(".tab_perfume_title").innerText = data['title'];
     // note 이름 불러오기
-    append_notes(data);
+    append_detail_notes(data);
 }
 // 2-1. note 의 name 불러오는 함수 * 
-function append_notes(data){
+function append_detail_notes(data){
     note_type_list = ["top", 'heart', 'base', 'none']
     note_type_list.forEach(note_type => {
         const note_data = data[note_type+'_notes'];
@@ -122,93 +128,77 @@ function perfume_review_tab(data){
     element.querySelector(".tab_review_count").innerText = data['perfume_reviews'].length;
     
     let avg_grade_star = document.getElementById("avg_grade_star");
-    avg_grade_star.innerHTML = '';
-    avg_grade = document.createElement("div");
-    avg_grade.className = "starpoint_wrap";
-    avg_grade.innerHTML = `
-                        <div class="starpoint_box star_${data['avg_grade']*20}">
-                        <label for="starpoint_1" class="label_star" title="0.5"><span class="blind">0.5점</span></label>
-                        <label for="starpoint_2" class="label_star" title="1"><span class="blind">1점</span></label>
-                        <label for="starpoint_3" class="label_star" title="1.5"><span class="blind">1.5점</span></label>
-                        <label for="starpoint_4" class="label_star" title="2"><span class="blind">2점</span></label>
-                        <label for="starpoint_5" class="label_star" title="2.5"><span class="blind">2.5점</span></label>
-                        <label for="starpoint_6" class="label_star" title="3"><span class="blind">3점</span></label>
-                        <label for="starpoint_7" class="label_star" title="3.5"><span class="blind">3.5점</span></label>
-                        <label for="starpoint_8" class="label_star" title="4"><span class="blind">4점</span></label>
-                        <label for="starpoint_9" class="label_star" title="4.5"><span class="blind">4.5점</span></label>
-                        <label for="starpoint_10" class="label_star" title="5"><span class="blind">5점</span></label>
-                        <span class="starpoint_bg"></span>
-                        </div>
-                         `;
-    avg_grade_star.append(avg_grade);
+    avg_grade_star.querySelector(".review_avg_grade_star").innerText = data['avg_grade'];
+    avg_grade_star.querySelector(".starpoint_bg").style=`width:${data['avg_grade']*20}%;`
     perfume_review_tab_review_list(data['perfume_reviews']);
 }
 // 3-2. 리뷰 탭 - 이 제품에 작성된 리뷰 보여주기
 function perfume_review_tab_review_list(review_data){
     let review_list_tab = document.getElementById("review_list_tab");
-    review_list_tab.innerHTML = '';
-    review_data.forEach(data => {
-        let review_list = document.createElement('div');
-        review_list.className = 'container_review_body';
-        review_list.id = 'review_'+data['id'];
-        review_list.innerHTML = `
-            <div class="sec_review_head">
-                <div class="sec_review_userinfo">
-                    <div class="review_user_info">
-                        <div class="review_username">
-                        ${data['user']}
+    if(review_data.length){
+        review_list_tab.innerHTML = '';
+        review_data.forEach(data => {
+            let review_list = document.createElement('div');
+            review_list.className = 'review_card';
+            review_list.id = 'review_'+data['id'];
+            review_list.innerHTML = `
+                <div class="review_card_head">
+                    <div class="review_head_userinfo">
+                        <div class="review_user_info">
+                            <div class="review_username">
+                            ${data['user']}
+                            </div>
+                            <div class="review_created_time">${data['created_at'].split('T')[0]}</div>
                         </div>
-                        <div class="review_created_time">${data['created_at'].split('T')[0]}</div>
+                        <div class="review_star_grade">
+                            <div class="starpoint_wrap">
+                                <div class="starpoint_box star_${data['grade']*20}">
+                                <label for="starpoint_1" class="label_star" title="0.5"><span class="blind">0.5점</span></label>
+                                <label for="starpoint_2" class="label_star" title="1"><span class="blind">1점</span></label>
+                                <label for="starpoint_3" class="label_star" title="1.5"><span class="blind">1.5점</span></label>
+                                <label for="starpoint_4" class="label_star" title="2"><span class="blind">2점</span></label>
+                                <label for="starpoint_5" class="label_star" title="2.5"><span class="blind">2.5점</span></label>
+                                <label for="starpoint_6" class="label_star" title="3"><span class="blind">3점</span></label>
+                                <label for="starpoint_7" class="label_star" title="3.5"><span class="blind">3.5점</span></label>
+                                <label for="starpoint_8" class="label_star" title="4"><span class="blind">4점</span></label>
+                                <label for="starpoint_9" class="label_star" title="4.5"><span class="blind">4.5점</span></label>
+                                <label for="starpoint_10" class="label_star" title="5"><span class="blind">5점</span></label>
+                                <span class="starpoint_bg"></span>
+                                </div>
+                            </div>
+                            <span class="review_user_grade">${data['grade']}</span>
+                        </div>
                     </div>
-                    <div class="review_star_grade">
-                        <div class="starpoint_wrap">
-                            <div class="starpoint_box star_${data['grade']*20}">
-                            <label for="starpoint_1" class="label_star" title="0.5"><span class="blind">0.5점</span></label>
-                            <label for="starpoint_2" class="label_star" title="1"><span class="blind">1점</span></label>
-                            <label for="starpoint_3" class="label_star" title="1.5"><span class="blind">1.5점</span></label>
-                            <label for="starpoint_4" class="label_star" title="2"><span class="blind">2점</span></label>
-                            <label for="starpoint_5" class="label_star" title="2.5"><span class="blind">2.5점</span></label>
-                            <label for="starpoint_6" class="label_star" title="3"><span class="blind">3점</span></label>
-                            <label for="starpoint_7" class="label_star" title="3.5"><span class="blind">3.5점</span></label>
-                            <label for="starpoint_8" class="label_star" title="4"><span class="blind">4점</span></label>
-                            <label for="starpoint_9" class="label_star" title="4.5"><span class="blind">4.5점</span></label>
-                            <label for="starpoint_10" class="label_star" title="5"><span class="blind">5점</span></label>
-                            <span class="starpoint_bg"></span>
+                </div>
+                <div class="review_card_body">
+                    <div class="review_body_content">
+                        <div class="review_good">
+                            <div class="review_good_image">
+                                <i id="emoji_good" class="bi bi-emoji-smile"></i>
+                            </div>
+                            <div class="review_good_content">
+                                ${data['good_content']}
                             </div>
                         </div>
-                        <span class="review_user_grade">${data['grade']}</span>
+                        <div class="review_bad">
+                            <div class="review_bad_image">
+                                <i id="emoji_bad" class="bi bi-emoji-frown"></i>
+                            </div>
+                            <div class="review_bad_content">
+                                ${data['bad_content']}
+                            </div>
+                        </div>
                     </div>
+                    ${data['image'] ? `<div class="review_result_image_box"><img class="review_result_image" src="http://127.0.0.1:8000${data['image']}" alt="No Image"></div>` : ""}
                 </div>
-            </div>
-            <div class="sec_review_body">
-                <div class="review_body_content">
-                    <div class="review_good">
-                        <div class="review_good_image">
-                            <i id="emoji_good" class="bi bi-emoji-smile"></i>
-                        </div>
-                        <div class="review_good_content">
-                            ${data['good_content']}
-                        </div>
-                    </div>
-                    <div class="review_bad">
-                        <div class="review_bad_image">
-                            <i id="emoji_bad" class="bi bi-emoji-frown"></i>
-                        </div>
-                        <div class="review_bad_content">
-                            ${data['bad_content']}
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div class="review_result_image_box">
-                        <img class="review_result_image" src="http://127.0.0.1:8000${data['image']}" alt="No Image">
-                    </div>
-                </div>
-            </div>
-            <hr/>
-        `;
-        review_list_tab.append(review_list);
-    });
+            `;
+            review_list_tab.append(review_list);
+        });
+    }else{
+        review_list_tab.innerHTML =`
+            <p class="none_result_list">아직 작성된 리뷰가 없습니다.</p>
+        `
+    }
 }
 
 
@@ -241,7 +231,7 @@ function perfume_recommend_tab(recommend_data){
     recommend_list.innerHTML = '';
     recommend_data.forEach(data => {
         let new_recommend_list = document.createElement('div');
-        new_recommend_list.className = 'col-lg-4 col-md-4 col-6';
+        new_recommend_list.className = 'col-lg-3 col-md-4 col-6';
         new_recommend_list.id = 'perfume_'+data['id'];
         new_recommend_list.innerHTML = `
             <a href="/perfume.html?perfume=${data['id']}">
@@ -256,15 +246,7 @@ function perfume_recommend_tab(recommend_data){
                             <p class="item_card_editor"><span class="brand">${data['brand']}</span></p>
                             <p class="item_card_title"><span class="title">${data['title']}</span></p>
                             <p class="item_card_tag">
-                                <span class="tag">#시트러스</span>
-                                <span class="tag">#백합</span>
-                                <span class="tag">#솔방울</span>
-                                <span class="tag">#시트러스</span>
-                                <span class="tag">#백합</span>
-                                <span class="tag">#솔방울</span>
-                                <span class="tag">#시트러스</span>
-                                <span class="tag">#백합</span>
-                                <span class="tag">#솔방울</span>
+                                ${append_notes(data)}
                             </p>
                         </div>
                     </div>
@@ -273,6 +255,18 @@ function perfume_recommend_tab(recommend_data){
         `;
         recommend_list.append(new_recommend_list);
     });
+}
+function append_notes(data){
+    note_type_list = ["top", 'heart', 'base', 'none']
+    let note_html = ``
+    let count =0;
+    note_type_list.forEach(note_type => {
+        const note_data = data[note_type+'_notes'];
+        note_data.forEach(note => {
+            note_html +=`<span class="tag">#${note['name']}</span>`;
+        });
+    });
+    return note_html
 }
 
 
